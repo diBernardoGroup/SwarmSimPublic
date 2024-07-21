d0ff34bec54f44cf2bad5752bc508d70a0452521
Andrea Giusti <60739241+andreagiusti96@users.noreply.github.com>
2024-07-21 17:11:08 +0200

    plot_spatial_inputs can record video

--- DOME/plot_spatial_inputs.m
+++ DOME/plot_spatial_inputs.m
@@ -15,33 +15,19 @@ inputs = sim_data.Environment.Inputs;
 xVec = sim_data.xVec;
 data_folder = sim_data.data_folder;
 
-Render.time_plot = 0:45:Simulation.Tmax;
-Render.all_time  = 0:1:Simulation.Tmax;
+make_videos = true;
+% Render.time_plot = 0:45:Simulation.Tmax;
+Render.time_plot = [];
+% Render.all_time  = 0:Simulation.deltaT:Simulation.Tmax;
+Render.all_time  = 0:10:Simulation.Tmax;
 Render.window = [-Simulation.arena(1),Simulation.arena(1),-Simulation.arena(2),Simulation.arena(2)]/2; % size of the simulation window
 
 exp_setup_time = 30;     % initial time window to be discarded from the experiment [s] (set to 30 for BCL, 0 for other exp)
-n_bins         = 5;     % number of bins for light distribution (set to 5 for BCL, 3 for other exp)
-
-
-% Render.window = [-Simulation.arena(1),Simulation.arena(1),-Simulation.arena(2),Simulation.arena(2)]/2; % size of the simulation window
-% Render.drawON=false;                % draw swarm during simulation (if N is large slows down the simulation)
-% Render.drawTraj=0;                  % draw trajectories of the agents (if N is large slows down the simulation)
-% Render.recordVideo=false;           % record video of the simulation (if true drawON must be true)
-% Render.frameRate = 1/Simulation.deltaT;
-% Render.agentShape = "rod";          % shape to plot the agents "rod" or any defualt marker key ('.','+','diamond',...)
-% Render.agentSize = 30;
-% Render.all_time = 0:Simulation.deltaT:Simulation.Tmax;
-% Render.shaded = true;
-%
-% % Light palette - red inputs
-% Render.agentsColor = [0 0 1]; % blue
-% Render.sim_c =      [0 0 0]; % black
-% Render.exp_c =      [0 0 1]; % blue
-% Render.cmap_inputs = linspace2([1,1,1], [1,0.5,0.5], 100)';  % light red
+n_bins         = 5;      % number of bins for light distribution (set to 5 for BCL, 3 for other exp)
 
 output_path = sim_folder;
 
-%% Plot the position of each agent at specified time instants (Time_plot)
+%% Plot at selected time instants (Render.time_plot)
 %%%both in silico and in vivo. Also, it plots the comparison between real and simulated histograms
 
 for i=1:length(Render.time_plot)
@@ -126,7 +112,7 @@ for i=1:length(Render.time_plot)
     end
     
     
-    %%%%%%%%%%%%%%%%%%%%%%%%% HISTOGTAM COMPARISON %%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+    %%%%%%%%%%%%%%%%%%%%%%%%% HISTOGTAM COMPARISON %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     figure("Position",[500+500 100+130 500 250])
     %Compute TVD between the distributions
@@ -154,20 +140,73 @@ for i=1:length(Render.time_plot)
     
 end
 
-%% Plot at each time instant
-%%%%%%%%%%%%%%%%%%%%%%%%% Photo-accumulation Index comp %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Plot at each time instant (Render.all_time)
+
+% plot PhAI in time
+fig_phai=figure("Position",[500 100 500 300]);
+fig_phai.Color = 'w';
+hold on;
+xlabel('Time [s]','FontSize',14)
+ylabel('PhAI','FontSize',14)
+xlim([0, Simulation.Tmax]);
+ylim([-0.6,0.6])
+yticks([-0.6:0.2:0.6])
+box on
+grid on
+% make video file
+if make_videos
+    video_phai = VideoWriter(fullfile(output_path, 'video_phai'),'MPEG-4');
+    video_phai.FrameRate = Render.frameRate;
+    open(video_phai);
+end
+
+% plot TVD in time
+fig_tvd=figure("Position",[500 100+300 500 300]);
+fig_tvd.Color = 'w';
+hold on;
+xlabel('Time [s]','FontSize',14)
+ylabel('TVD','FontSize',14)
+xlim([0, Simulation.Tmax]);
+ylim([0,0.15])
+box on
+grid on
+% make video file
+if make_videos
+    video_tvd = VideoWriter(fullfile(output_path, 'video_tvd'),'MPEG-4');
+    video_tvd.FrameRate = Render.frameRate;
+    open(video_tvd);
+end
+
+fig_light_dist=figure("Position",[500 100+300*2 500 300]);
+fig_light_dist.Color = 'w';
+hold on;
+xlabel('Input intensity','FontSize',14)
+ylabel('Density of agents','FontSize',14)
+ylim([0,0.4])
+xlim([-0.1,1.1])
+xticks(0:1/n_bins:1)
+box
+% make video file
+if make_videos
+    video_light_dist = VideoWriter(fullfile(output_path, 'video_diff_light_dist'),'MPEG-4');
+    video_light_dist.FrameRate = Render.frameRate;
+    open(video_light_dist);
+end
+
+% allocate vars
+norm_slope_exp = nan(size(Render.all_time));
+norm_slope_sim = nan(size(Render.all_time));
+tvd            = nan(size(Render.all_time));
 
 for i=1:length(Render.all_time)
-    %Comput PhI at every time instant
-    
-    % simulation data
+    % load simulation data
     frame_index = round(Render.all_time(i) / Simulation.deltaT) + 1;
     [x_cur,indices_inWindow] = getInWindow(squeeze(xVec(frame_index,:,:)), Render.window);
-
-    [density_by_input_sim] = agentsDensityByInput(Environment.Inputs.Points, Environment.Inputs.Values, x_cur, Render.window, n_bins);
+    
+    [density_by_input_sim,bins] = agentsDensityByInput(Environment.Inputs.Points, Environment.Inputs.Values, x_cur, Render.window, n_bins);
     [~,norm_slope_sim(i),~] = linearDependence((bins(1:end-1)+bins(2:end))'/2, density_by_input_sim');
-
-    % combo exp data
+    
+    % load and combo exp data
     for j=1:length(experiments_names)   % for each replicate
         time_to_plot_exp = Render.all_time(i)+exp_setup_time;
         experiments_names(j) = strrep(experiments_names(j),'_E_','_Euglena_');
@@ -191,41 +230,71 @@ for i=1:length(Render.all_time)
     density_by_input_exp_mean = sum(squeeze(agents_by_input)./squeeze(pixels_by_input),1);
     density_by_input_exp_mean = density_by_input_exp_mean/sum(density_by_input_exp_mean);
     [~,norm_slope_exp(i),~] = linearDependence((bins(1:end-1)+bins(2:end))'/2, density_by_input_exp_mean');
-
+    
     %Get density of experimental data
     tvd(i) = 0.5 * norm(density_by_input_exp_mean-density_by_input_sim,1); % Total Variation Distance
+    
+    
+    if make_videos || i==length(Render.all_time)
+        
+        % plot the in silico and in vivo PhAI to compare the settling time
+        figure(fig_phai)
+        cla
+        plot(Render.all_time,norm_slope_exp,'LineWidth',2,'Color',Render.exp_c);
+        plot(Render.all_time,norm_slope_sim,'LineWidth',2,'Color',Render.sim_c);
+        % legend({'REAL','SIMULATED'},'FontSize',14)
+        legend({'In vivo','In silico'},'FontSize',14,'FontAngle','italic')
+        % save frame
+        if make_videos
+            currFrame = getframe(fig_phai);
+            writeVideo(video_phai,currFrame);
+        end
+        
+        % plot TVD in time
+        figure(fig_tvd)
+        cla
+        plot(Render.all_time,tvd,'LineWidth',2,'Color', [1 1 1]*0.5);
+        % save frame
+        if make_videos
+            currFrame = getframe(fig_tvd);
+            writeVideo(video_tvd,currFrame);
+        end
+        
+        % plot histograms distributions with respect to light intensity
+        figure(fig_light_dist)
+        cla
+        b_exp = bar((bins(1:end-1)+bins(2:end))/2,density_by_input_exp_mean, 1, FaceColor = 'b', FaceAlpha = 0.5);
+        b_sim = bar((bins(1:end-1)+bins(2:end))/2,density_by_input_sim, 1, FaceColor = 'k', FaceAlpha = 0.4);
+        %legend({'REAL','SIMULATED'},'FontSize',14)
+        legend({'In vivo','In silico'},'FontSize',14,'FontAngle','italic')
+        text(mean(bins),0.35,['TVD=',num2str(tvd(i),'%.2f')],'HorizontalAlignment','center','FontSize',14)
+        % save frame
+        if make_videos
+            currFrame = getframe(fig_light_dist);
+            writeVideo(video_light_dist,currFrame);
+        end
+        
+    end
 end
 
-% plot the in silico and in vivo PhAI to compare the settling time
-figure("Position",[500 100+150 500 300])
-hold on;
-plot(Render.all_time,norm_slope_exp,'LineWidth',2,'Color',Render.exp_c);
-plot(Render.all_time,norm_slope_sim,'LineWidth',2,'Color',Render.sim_c);
-legend({'REAL','SIMULATED'},'FontSize',14)
-xlabel('Time [s]','FontSize',14)
-ylabel('PhAI','FontSize',14)
-xlim([0, Simulation.Tmax]);
-box on
-grid on
+% terminate videos
+if make_videos
+    close(video_phai);
+    close(video_tvd);
+    close(video_light_dist);
+end
+
+% save figures at the last time step
 if output_path
-    fig=gcf; fig.Units = fig.PaperUnits; fig.PaperSize = fig.Position(3:4); % set correct pdf size
-    saveas(gcf, fullfile(output_path, 'photo_acc_index'))
-    saveas(gcf, fullfile(output_path, 'photo_acc_index'),'pdf')
+    fig_phai.Units = fig_phai.PaperUnits; fig_phai.PaperSize = fig_phai.Position(3:4); % set correct pdf size
+    saveas(fig_phai, fullfile(output_path, "photo_acc_index"))
+    saveas(fig_phai, fullfile(output_path, "photo_acc_index"),'pdf')
+    
+    fig_tvd.Units = fig_tvd.PaperUnits; fig_tvd.PaperSize = fig_tvd.Position(3:4); % set correct pdf size
+    saveas(fig_tvd, fullfile(output_path, 'TVD'))
+    saveas(fig_tvd, fullfile(output_path, 'TVD'),'pdf')
+    
+    fig_light_dist.Units = fig_light_dist.PaperUnits; fig_light_dist.PaperSize = fig_light_dist.Position(3:4); % set correct pdf size
+    saveas(fig_light_dist, fullfile(output_path, 'difference_light_distribution'))
+    saveas(fig_light_dist, fullfile(output_path, 'difference_light_distribution'),'pdf')
 end
-
-% plot TVD in time
-figure("Position",[500 100+450 500 300])
-plot(Render.all_time,tvd,'LineWidth',2,'Color', [1 1 1]*0.5);
-xlabel('Time [s]','FontSize',14)
-ylabel('TVD','FontSize',14)
-xlim([0, Simulation.Tmax]);
-ylim([0,0.15])
-box on
-grid on
-if output_path
-    fig=gcf; fig.Units = fig.PaperUnits; fig.PaperSize = fig.Position(3:4); % set correct pdf size
-    saveas(gcf, fullfile(output_path, 'TVD'))
-    saveas(gcf, fullfile(output_path, 'TVD'),'pdf')
-end
-
-
--- Graphics/makeVideo.m
+++ Graphics/makeVideo.m
@@ -15,15 +15,20 @@ open(video);
 % init figure
 fig=figure;
 fig.Color = 'w';
+fig.Position = [100 100 2000 1500];
+
 [x,indices_inWindow] = getInWindow(squeeze(xVec(1,:,:)), Render.window);
 x_pre = squeeze(2*xVec(1,indices_inWindow,:)-xVec(2,indices_inWindow,:));
-    
+
 % plot swarm
 plotSwarmInit(x, 0, inf, inf, Render.window, [Render.window(2)-Render.window(1), Render.window(4)-Render.window(3)]/2, false, false, false, Render.agentShape, Render.agentSize, Render.agentsColor, x_pre);
 makeFigureSwarm(x, x_pre, 0,Environment, Render)
 
 % plot light distribution
 % makeFigureDensity(x,0,Environment,Render)
+
+% plot experimental image
+% makeFigureExperiment(0, Render)
 
 % save frame
 currFrame = getframe(gcf);
@@ -36,14 +41,17 @@ for i=2:time_instants
     %clear figure
     cla
     t = (i-1)*deltaT;
-    [x,indices_inWindow] = getInWindow(squeeze(xVec(i,:,:)), Render.window);
-    x_pre=squeeze(xVec(i-1,indices_inWindow,:));
+        [x,indices_inWindow] = getInWindow(squeeze(xVec(i,:,:)), Render.window);
+        x_pre=squeeze(xVec(i-1,indices_inWindow,:));
     
-    % plot current positions and inputs
-     makeFigureSwarm(x, x_pre, t,Environment, Render)
+    plot current positions and inputs
+    makeFigureSwarm(x, x_pre, t,Environment, Render)
     
     % plot light distribution
     % makeFigureDensity(x,t,Environment,Render)
+    
+    % plot experimental image
+    % makeFigureExperiment(t, Render)
     
     % save frame
     currFrame = getframe(gcf);
@@ -56,32 +64,80 @@ close(video);
 
 end
 
+%% Functions to print a single frame
+% print frame from simulation
 function [] = makeFigureSwarm(x, x_pre, t,Environment, Render)
-    if isfield(Environment,'Inputs') && isfield(Environment.Inputs,'Points')
-        plotEnvField(Environment.Inputs.Points, Environment.Inputs.Values, Render.window, Render.cmap_inputs)
-    end
-    if Render.drawTraj; plotTrajectory(xVec, false, [0,0.7,0.9], Render.drawTraj); end
-    if isfield(Environment,'boundary'); plotBoundary(Environment.boundary); end
-    plotSwarm(x, t, inf, inf, false, ones(size(x,1), 1), false, Render.agentShape, Render.agentSize, Render.agentsColor, x_pre);
+if isfield(Environment,'Inputs') && isfield(Environment.Inputs,'Points')
+    plotEnvField(Environment.Inputs.Points, Environment.Inputs.Values, Render.window, Render.cmap_inputs)
+end
+if Render.drawTraj; plotTrajectory(xVec, false, [0,0.7,0.9], Render.drawTraj); end
+if isfield(Environment,'boundary'); plotBoundary(Environment.boundary); end
+plotSwarm(x, t, inf, inf, false, ones(size(x,1), 1), false, Render.agentShape, Render.agentSize, Render.agentsColor, x_pre);
 end
 
+% print light distribution from simulation
 function [] = makeFigureDensity(x, t, Environment, Render)
-    % simulation light distribution
-    [density_by_input_sim, bins, norm_slope_sim, c_coeff_sim, coefficents, ~,~, u_values_sim] = agentsDensityByInput(Environment.Inputs.Points, Environment.Inputs.Values, x, Render.window, 5);
+[density_by_input_sim, bins] = agentsDensityByInput(Environment.Inputs.Points, Environment.Inputs.Values, x, Render.window, 5);
 
-    title(sprintf('t=%.2fs',t))
-    bar((bins(1:end-1)+bins(2:end))/2,density_by_input_sim, 1, 'FaceColor', [1 1 1]*0.5)
-    hold on
-    plot(bins,coefficents(1)+coefficents(2)*bins,LineWidth=2);
-    xlabel('Input intensity')
-    ylabel('Density of agents')
-    yticks([0:0.2:1]);
-    %text(max(bins),max(density_by_input_sim)*1.1,['\rho=',num2str(c_coeff_sim,'%.2f')],'HorizontalAlignment','right','FontSize',14)
-    text(max(bins),0.35,['PhAI=',num2str(norm_slope_sim,'%.2f')],'HorizontalAlignment','right','FontSize',14)
-    %ylim([0,max(density_by_input_sim)*1.15])
-    ylim([0,0.4])
-    xlim([-0.1,1.1])
-    xticks(round(bins,2))
+title(sprintf('t=%.2fs',t))
+bar((bins(1:end-1)+bins(2:end))/2,density_by_input_sim, 1, 'FaceColor', [1 1 1]*0.5)
+hold on
+plot(bins,coefficents(1)+coefficents(2)*bins,LineWidth=2);
+xlabel('Input intensity')
+ylabel('Density of agents')
+yticks([0:0.2:1]);
+%text(max(bins),max(density_by_input_sim)*1.1,['\rho=',num2str(c_coeff_sim,'%.2f')],'HorizontalAlignment','right','FontSize',14)
+text(max(bins),0.35,['PhAI=',num2str(norm_slope_sim,'%.2f')],'HorizontalAlignment','right','FontSize',14)
+%ylim([0,max(density_by_input_sim)*1.15])
+ylim([0,0.4])
+xlim([-0.1,1.1])
+xticks(round(bins,2))
 end
 
+% print frame from experiment
+function [] = makeFigureExperiment(t, Render)
+experiments_folder = "/Volumes/DOMEPEN/Experiments";
+experiments_names = ["2023_07_10_E_30","2023_07_10_E_34","2023_07_10_E_35"];
+brightness_thresh = 0.3;    % brightness threshold for object detection (spatial experiments)
+background_sub = true;      % use background subtraction for object detection (spatial experiments)
+pattern_blurring = 15;      % blurring of the spatial input pattern (spatial experiments)
+exp_setup_time = 30;     % initial time window to be discarded from the experiment [s] (set to 30 for BCL, 0 for other exp)
 
+mask = cell(size(experiments_names));
+u = cell(size(experiments_names));
+
+% combo exp data
+for j=1:length(experiments_names)   % for each replicate
+    experiments_names(j) = strrep(experiments_names(j),'_E_','_Euglena_');
+    data_folder =  fullfile(experiments_folder,experiments_names(j));
+    
+    time_to_plot_exp = t+exp_setup_time;
+    mask{j} = detectObjects(data_folder, background_sub, brightness_thresh, time_to_plot_exp);
+    u{j} = loadInputPattern(data_folder, pattern_blurring, time_to_plot_exp);
+    assert( mean(abs(u{1} - imresize(u{j},size(u{1}))),'all')<0.1, 'Replicates have different inputs' )
+    
+    % get distribution wrt light intensity
+    %         [~, ~, agents_by_input(j,:), pixels_by_input(j,:)] = agentsDensityByInput(inputs.Points, inputs.Values, mask{j}, Render.window, n_bins);
+    
+    % combination of masks over the replicates
+    if j==1
+        combo_mask = mask{1};
+    else
+        combo_mask = (combo_mask+mask{j})>=1;
+    end
+end
+
+%     % weighted average light distribution over the replicates
+%     density_by_input_exp_mean = sum(squeeze(agents_by_input)./squeeze(pixels_by_input),1);
+%     density_by_input_exp_mean = density_by_input_exp_mean/sum(density_by_input_exp_mean);
+%     [~,norm_slope_exp(i),~] = linearDependence((bins(1:end-1)+bins(2:end))'/2, density_by_input_exp_mean');
+
+x_vec = linspace(Render.window(1),Render.window(2),size(combo_mask,2));
+y_vec = linspace(Render.window(3),Render.window(4),size(combo_mask,1));
+%Plot the inputs
+imagesc(x_vec,y_vec,flip(u{1}'))
+%Plot the agents detected
+I=imagesc(x_vec,y_vec,cat(3,Render.agentsColor(1).*combo_mask,Render.agentsColor(2).*combo_mask,Render.agentsColor(3).*combo_mask));
+set(I, 'AlphaData', combo_mask);
+title(sprintf('t=%.2fs',time_to_plot_exp))
+end
