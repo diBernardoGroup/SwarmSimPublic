e8d3097df00f22525c377f39c3570282750f5aa8
Andrea Giusti <60739241+andreagiusti96@users.noreply.github.com>
2024-08-01 13:22:58 +0200

    improved detectObjects with brightness scaling

--- DOME/detectObjects.m
+++ DOME/detectObjects.m
@@ -1,4 +1,4 @@
-function mask = detectObjects(data_folder, background_sub, brightness_thresh,timeInstant)
+function mask = detectObjects(data_folder, background_sub, brightness_thresh,timeInstant, brightness_scaling)
 
 
 arguments
@@ -6,6 +6,7 @@ arguments
     background_sub        logical
     brightness_thresh     double 
     timeInstant           double        = 180
+    brightness_scaling    logical       = true
 end
 
 
@@ -18,7 +19,16 @@ end
     end
     
     img_grey = double(img(:,:,1))/255;
-
+    
+    % adjust brightness to use the full dynamic range
+    if brightness_scaling
+    min_val = min(img_grey(360:720,640:1280),[],'all');
+    img_grey = max(min_val, img_grey);
+    img_grey = img_grey - min_val;
+    max_val = max(img_grey(360:720,640:1280),[],'all');
+    img_grey = img_grey/max_val;
+    end
+    
     % background subtraction
     if background_sub
         background = double(imread(fullfile(data_folder,'background.jpeg')))/255;
